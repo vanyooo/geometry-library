9ac9959e9328d0951baebe8f463a53376fc72025
Ivan <zimin.ivann.1998@gmail.com>
2026-05-14 12:07:27 +0300

    change ShapeComparator

--- GeometryUtil/src/main/java/com/zimin/ShapeComparator.java
+++ GeometryUtil/src/main/java/com/zimin/ShapeComparator.java
@@ -5,6 +5,7 @@ public class ShapeComparator {
 
     public static Shape findLargerArea(Shape first, Shape second) {
 
+        System.out.println("Comparison");
         if (first.getArea() > second.getArea()) {
             return first;
         }
